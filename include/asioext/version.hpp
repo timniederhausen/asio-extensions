92af73e981ac19f7b77d60988f35018f2bebae97
Tim Niederhausen <tim@rnc-ag.de>
2017-02-25 05:50:26 +0100

    Add version header

---
+++ include/asioext/version.hpp
@@ -0,0 +1,18 @@
+/// @file
+/// Define the asioext version macros.
+///
+/// @copyright Copyright (c) 2017 Tim Niederhausen (tim@rnc-ag.de)
+/// Distributed under the Boost Software License, Version 1.0.
+/// (See accompanying file LICENSE_1_0.txt or copy at
+/// http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef ASIOEXT_VERSION_HPP
+#define ASIOEXT_VERSION_HPP
+
+// ASIOEXT_VERSION % 100 is the sub-minor version
+// ASIOEXT_VERSION / 100 % 1000 is the minor version
+// ASIOEXT_VERSION / 100000 is the major version
+#define ASIOEXT_VERSION 100 // 0.1.0
+#define ASIOEXT_VERSION_STR 0.1.0
+
+#endif
