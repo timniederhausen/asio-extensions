2845918afc231ee622e4e36dce51f90d6558c30d
Tim Niederhausen <tim@rnc-ag.de>
2018-05-16 23:34:58 +0200

    Bump version

--- include/asioext/version.hpp
+++ include/asioext/version.hpp
@@ -12,7 +12,7 @@
 // ASIOEXT_VERSION % 100 is the sub-minor version
 // ASIOEXT_VERSION / 100 % 1000 is the minor version
 // ASIOEXT_VERSION / 100000 is the major version
-#define ASIOEXT_VERSION 100 // 0.2.0
-#define ASIOEXT_VERSION_STR 0.2.0
+#define ASIOEXT_VERSION 100 // 0.3.0
+#define ASIOEXT_VERSION_STR 0.3.0
 
 #endif
